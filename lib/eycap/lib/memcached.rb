589a11e749030a3b7dc7367f4bfb91375baa9876
shai <shai@shai-rosenfelds-macbook.local>
2008-07-02 17:34:49 +0300

    ruby code

--- lib/eycap/lib/memcached.rb
+++ lib/eycap/lib/memcached.rb
@@ -4,8 +4,9 @@ module Capistrano
   class Memcached
 
     #called in capistrano recipie like so:
-    # Memcached.flush(11211,11212) or Memcached.flush(11211) etc
+    # Capistrano::Memcached.flush(11211,11212) or Capistrano::Memcached.flush(11211) etc
     def self.flush(*ports)
+      ports = [ 11211 ] if ports.empty? #default value
       for port in ports
 				socket = TCPSocket.new( '127.0.0.1', port)
 				socket.write( "flush_all\n" )
@@ -19,6 +20,8 @@ module Capistrano
 				end
 				socket.close
       end
+      rescue
+        puts "an error occured while flushing memcached."
     end
   end
 
--- lib/eycap/recipes/memcached.rb
+++ lib/eycap/recipes/memcached.rb
@@ -20,6 +20,10 @@ Capistrano::Configuration.instance(:must_exist).load do
     task :restart, :roles => :app, :only => {:memcached => true} do
       sudo "/etc/init.d/memcached restart"
     end        
+    desc "flush memcached."
+    task :flush, :roles => :app, :only => {:memcached => true} do 
+      Capistrano::Memcached.flush(11211)      
+    end
     desc "Symlink the memcached.yml file into place if it exists"
     task :symlink_configs, :roles => :app, :only => {:memcached => true }, :except => { :no_release => true } do
       run "if [ -f #{shared_path}/config/memcached.yml ]; then ln -nfs #{shared_path}/config/memcached.yml #{latest_release}/config/memcached.yml; fi"
